df18d5d4918ea1651c867a76ed08e3b03a301aeb
German <karabastov@yahoo.com>
2026-06-02 16:25:38 +0300

    Visual recoil function from realrtcw

--- code/cgame/cg_weapons.c
+++ code/cgame/cg_weapons.c
@@ -4375,65 +4375,63 @@ CG_WeaponFireRecoil
 ==============
 */
 void CG_WeaponFireRecoil( int weapon ) {
-//	const	vec3_t maxKickAngles = {25, 30, 25};
 	float pitchRecoilAdd, pitchAdd;
 	float yawRandom;
 	vec3_t recoil;
-	//
 	pitchRecoilAdd = 0;
 	pitchAdd = 0;
 	yawRandom = 0;
-	//
+
+	if ( cg_thirdPerson.integer ) {
+		return;
+	}
+	
 	switch ( weapon ) {
 	case WP_LUGER:
 	case WP_SILENCER:
 	case WP_COLT:
-	case WP_AKIMBO: //----(SA)	added
-		//pitchAdd = 2+rand()%3;
-		//yawRandom = 2;
-		break;
+	case WP_AKIMBO:
+	   yawRandom = 0.5;
+	   pitchRecoilAdd = 2;
+	   pitchAdd = 1;
+	break;
+    break;
 	case WP_MAUSER:
 	case WP_GARAND:
-		//pitchAdd = 4+rand()%3;
-		//yawRandom = 4;
-		pitchAdd = 2;   //----(SA)	for DM
-		yawRandom = 1;  //----(SA)	for DM
-		break;
+		pitchAdd = 1;
+		yawRandom = 1; 
+	    pitchRecoilAdd = 1.5;   
+	break;
 	case WP_SNIPERRIFLE:
 	case WP_SNOOPERSCOPE:
-		pitchAdd = 0.6;
-		break;
-	case WP_FG42SCOPE:
-	case WP_FG42:
+		pitchAdd = 0.8;
+	break;
 	case WP_MP40:
 	case WP_THOMPSON:
 	case WP_STEN:
-		//pitchRecoilAdd = 1;
-		pitchAdd = 1 + rand() % 3;
-		yawRandom = 2;
-
-
-		pitchAdd *= 0.3;
-		yawRandom *= 0.3;
-		break;
+		pitchAdd = 1;
+		pitchRecoilAdd = 1;  
+		yawRandom = 1;
+	break;
+	case WP_FG42SCOPE:
+		pitchAdd = 0.8;
+	break;
+	case WP_FG42:
+		pitchAdd = 1;
+		pitchRecoilAdd = 1;   
+		yawRandom = 1;  
+	break;
 	case WP_PANZERFAUST:
-		//pitchAdd = 12+rand()%3;
-		//yawRandom = 6;
-
 		CG_StartShakeCamera( 0.05, 700, cg.snap->ps.origin, 100 );
-
-		// push the player back instead
 		break;
 	case WP_VENOM:
-		pitchRecoilAdd = pow( random(),8 ) * ( 10 + VectorLength( cg.snap->ps.velocity ) / 5 );
+		pitchRecoilAdd = pow( random(),8 ) * ( 5 + VectorLength( cg.snap->ps.velocity ) / 5 );
 		pitchAdd = ( rand() % 5 ) - 2;
-		yawRandom = 2;
-
-
+		yawRandom = 1;
 		pitchRecoilAdd *= 0.5;
 		pitchAdd *= 0.5;
 		yawRandom *= 0.5;
-		break;
+	break;
 	default:
 		return;
 	}
@@ -4448,6 +4446,7 @@ void CG_WeaponFireRecoil( int weapon ) {
 	// set the recoil
 	cg.recoilPitch -= pitchRecoilAdd;
 }
+
 
 
 /*
