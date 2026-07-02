c86a10f214cb5b6263dce614d811bb90bf46ab7c
German <karabastov@yahoo.com>
2026-07-02 16:57:50 +0300

    Playerclip2

--- code/rend2/tr_shader.c
+++ code/rend2/tr_shader.c
@@ -1602,83 +1602,83 @@ typedef struct {
 infoParm_t infoParms[] = {
 	// server relevant contents
 
-//----(SA)	modified
-	{"clipmissile",  1,  0, CONTENTS_MISSILECLIP},       // impact only specific weapons (rl, gl)
-//----(SA)	end
+	//----(SA)	modified
+	{"clipmissile", 1, 0, CONTENTS_MISSILECLIP}, // impact only specific weapons (rl, gl)
+												 //----(SA)	end
 
-// RF, AI sight
-	{"ai_nosight",   1,  0,  CONTENTS_AI_NOSIGHT},
-	{"clipshot", 1,  0,  CONTENTS_CLIPSHOT},         // stops bullets
-// RF, end
+	// RF, AI sight
+	{"ai_nosight", 1, 0, CONTENTS_AI_NOSIGHT},
+	{"clipshot", 1, 0, CONTENTS_CLIPSHOT}, // stops bullets
+										   // RF, end
 
-	{"water",        1,  0,  CONTENTS_WATER },
-	{"slag",     1,  0,  CONTENTS_SLIME },       // uses the CONTENTS_SLIME flag, but the shader reference is changed to 'slag'
+	{"water", 1, 0, CONTENTS_WATER},
+	{"slag", 1, 0, CONTENTS_SLIME}, // uses the CONTENTS_SLIME flag, but the shader reference is changed to 'slag'
 	// to idendify that this doesn't work the same as 'slime' did.
 	// (slime hurts instantly, slag doesn't)
-//	{"slime",		1,	0,	CONTENTS_SLIME },		// mildly damaging
-	{"lava",     1,  0,  CONTENTS_LAVA },        // very damaging
-	{"playerclip",   1,  0,  CONTENTS_PLAYERCLIP },
-	{"monsterclip",  1,  0,  CONTENTS_MONSTERCLIP },
-	{"nodrop",       1,  0,  CONTENTS_NODROP },      // don't drop items or leave bodies (death fog, lava, etc)
-	{"nonsolid", 1,  SURF_NONSOLID,  0},                     // clears the solid flag
+	//	{"slime",		1,	0,	CONTENTS_SLIME },		// mildly damaging
+	{"lava", 1, 0, CONTENTS_LAVA}, // very damaging
+	{"playerclip", 1, 0, CONTENTS_PLAYERCLIP},
+	{"monsterclip", 1, 0, CONTENTS_MONSTERCLIP},
+	{"nodrop", 1, 0, CONTENTS_NODROP}, // don't drop items or leave bodies (death fog, lava, etc)
+	{"nonsolid", 1, SURF_NONSOLID, 0}, // clears the solid flag
 
 	// utility relevant attributes
-	{"origin",       1,  0,  CONTENTS_ORIGIN },      // center of rotating brushes
-	{"trans",        0,  0,  CONTENTS_TRANSLUCENT }, // don't eat contained surfaces
-	{"detail",       0,  0,  CONTENTS_DETAIL },      // don't include in structural bsp
-	{"structural",   0,  0,  CONTENTS_STRUCTURAL },  // force into structural bsp even if trnas
-	{"areaportal",   1,  0,  CONTENTS_AREAPORTAL },  // divides areas
-	{"clusterportal", 1,0,  CONTENTS_CLUSTERPORTAL },    // for bots
-	{"donotenter",  1,  0,  CONTENTS_DONOTENTER },       // for bots
+	{"origin", 1, 0, CONTENTS_ORIGIN},				 // center of rotating brushes
+	{"trans", 0, 0, CONTENTS_TRANSLUCENT},			 // don't eat contained surfaces
+	{"detail", 0, 0, CONTENTS_DETAIL},				 // don't include in structural bsp
+	{"structural", 0, 0, CONTENTS_STRUCTURAL},		 // force into structural bsp even if trnas
+	{"areaportal", 1, 0, CONTENTS_AREAPORTAL},		 // divides areas
+	{"clusterportal", 1, 0, CONTENTS_CLUSTERPORTAL}, // for bots
+	{"donotenter", 1, 0, CONTENTS_DONOTENTER},		 // for bots
 
 	// Rafael - nopass
-	{"donotenterlarge", 1, 0,    CONTENTS_DONOTENTER_LARGE }, // for larger bots
+	{"donotenterlarge", 1, 0, CONTENTS_DONOTENTER_LARGE}, // for larger bots
 
-	{"fog",          1,  0,  CONTENTS_FOG},          // carves surfaces entering
-	{"sky",          0,  SURF_SKY,       0 },        // emit light from an environment map
-	{"lightfilter",  0,  SURF_LIGHTFILTER, 0 },      // filter light going through it
-	{"alphashadow",  0,  SURF_ALPHASHADOW, 0 },      // test light on a per-pixel basis
-	{"hint",     0,  SURF_HINT,      0 },        // use as a primary splitter
+	{"fog", 1, 0, CONTENTS_FOG}, // carves surfaces entering
+	{"playerclip2", 1, 0, CONTENTS_PLAYERCLIP2},
+	{"sky", 0, SURF_SKY, 0},				 // emit light from an environment map
+	{"lightfilter", 0, SURF_LIGHTFILTER, 0}, // filter light going through it
+	{"alphashadow", 0, SURF_ALPHASHADOW, 0}, // test light on a per-pixel basis
+	{"hint", 0, SURF_HINT, 0},				 // use as a primary splitter
 
 	// server attributes
-	{"slick",            0,  SURF_SLICK,     0 },
-	{"noimpact",     0,  SURF_NOIMPACT,  0 },        // don't make impact explosions or marks
-	{"nomarks",          0,  SURF_NOMARKS,   0 },        // don't make impact marks, but still explode
-	{"ladder",           0,  SURF_LADDER,    0 },
-	{"nodamage",     0,  SURF_NODAMAGE,  0 },
+	{"slick", 0, SURF_SLICK, 0},
+	{"noimpact", 0, SURF_NOIMPACT, 0}, // don't make impact explosions or marks
+	{"nomarks", 0, SURF_NOMARKS, 0},   // don't make impact marks, but still explode
+	{"ladder", 0, SURF_LADDER, 0},
+	{"nodamage", 0, SURF_NODAMAGE, 0},
 
-	{"monsterslick", 0,  SURF_MONSTERSLICK,  0},     // surf only slick for monsters
+	{"monsterslick", 0, SURF_MONSTERSLICK, 0}, // surf only slick for monsters
 
-//	{"flesh",		0,	SURF_FLESH,		0 },
-	{"glass",        0,  SURF_GLASS,     0 },    //----(SA)	added
-	{"ceramic",      0,  SURF_CERAMIC,   0 },    //----(SA)	added
+	//	{"flesh",		0,	SURF_FLESH,		0 },
+	{"glass", 0, SURF_GLASS, 0},	 //----(SA)	added
+	{"ceramic", 0, SURF_CERAMIC, 0}, //----(SA)	added
 
 	// steps
-	{"metal",        0,  SURF_METAL,     0 },
-	{"metalsteps",   0,  SURF_METAL,     0 },    // retain bw compatibility with Q3A metal shaders... (SA)
-	{"nosteps",      0,  SURF_NOSTEPS,   0 },
-	{"woodsteps",    0,  SURF_WOOD,      0 },
-	{"grasssteps",   0,  SURF_GRASS,     0 },
-	{"gravelsteps",  0,  SURF_GRAVEL,    0 },
-	{"carpetsteps",  0,  SURF_CARPET,    0 },
-	{"snowsteps",    0,  SURF_SNOW,      0 },
-	{"roofsteps",    0,  SURF_ROOF,      0 },    // tile roof
+	{"metal", 0, SURF_METAL, 0},
+	{"metalsteps", 0, SURF_METAL, 0}, // retain bw compatibility with Q3A metal shaders... (SA)
+	{"nosteps", 0, SURF_NOSTEPS, 0},
+	{"woodsteps", 0, SURF_WOOD, 0},
+	{"grasssteps", 0, SURF_GRASS, 0},
+	{"gravelsteps", 0, SURF_GRAVEL, 0},
+	{"carpetsteps", 0, SURF_CARPET, 0},
+	{"snowsteps", 0, SURF_SNOW, 0},
+	{"roofsteps", 0, SURF_ROOF, 0}, // tile roof
 
-	{"rubble", 0, SURF_RUBBLE, 0 },
+	{"rubble", 0, SURF_RUBBLE, 0},
 
 	// drawsurf attributes
-	{"nodraw",       0,  SURF_NODRAW,    0 },    // don't generate a drawsurface (or a lightmap)
-	{"pointlight",   0,  SURF_POINTLIGHT, 0 },   // sample lighting at vertexes
-	{"nolightmap",   0,  SURF_NOLIGHTMAP,0 },        // don't generate a lightmap
-	{"nodlight", 0,  SURF_NODLIGHT, 0 },     // don't ever add dynamic lights
+	{"nodraw", 0, SURF_NODRAW, 0},		   // don't generate a drawsurface (or a lightmap)
+	{"pointlight", 0, SURF_POINTLIGHT, 0}, // sample lighting at vertexes
+	{"nolightmap", 0, SURF_NOLIGHTMAP, 0}, // don't generate a lightmap
+	{"nodlight", 0, SURF_NODLIGHT, 0},	   // don't ever add dynamic lights
 
-	{"monsterslicknorth",    0, SURF_MONSLICK_N,0},
-	{"monsterslickeast", 0, SURF_MONSLICK_E,0},
-	{"monsterslicksouth",    0, SURF_MONSLICK_S,0},
-	{"monsterslickwest", 0, SURF_MONSLICK_W,0}
+	{"monsterslicknorth", 0, SURF_MONSLICK_N, 0},
+	{"monsterslickeast", 0, SURF_MONSLICK_E, 0},
+	{"monsterslicksouth", 0, SURF_MONSLICK_S, 0},
+	{"monsterslickwest", 0, SURF_MONSLICK_W, 0}
 
 };
-
 
 /*
 ===============
